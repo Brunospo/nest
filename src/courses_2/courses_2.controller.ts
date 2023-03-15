1914addc6e51fe74a1f0a3963297d8be3e0abb18
Bruno Bispo <brunobispoo25@gmail.com>
2023-03-14 23:27:24 -0300

    feat: Added dto classes

--- src/courses_2/courses_2.controller.ts
+++ src/courses_2/courses_2.controller.ts
@@ -8,6 +8,8 @@ import {
   Put,
 } from '@nestjs/common';
 import { Courses2Service } from './courses_2.service';
+import { CreateCourseDto } from './dto/create_course.dto';
+import { UpdateCourseDto } from './dto/update_course.dto';
 
 @Controller('courses-2')
 export class Courses2Controller {
@@ -24,13 +26,13 @@ export class Courses2Controller {
   }
 
   @Post()
-  create(@Body() body) {
-    return this.coursesService.create(body);
+  create(@Body() createCourseDto: CreateCourseDto) {
+    return this.coursesService.create(createCourseDto);
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() body) {
-    return this.coursesService.update(id, body);
+  update(@Param('id') id: string, @Body() updateCourseDto: UpdateCourseDto) {
+    return this.coursesService.update(id, updateCourseDto);
   }
 
   @Delete(':id')
