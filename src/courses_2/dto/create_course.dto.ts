aa62c5145509619379ecbcbb8fc28e3c78a6e2ef
Bruno Bispo <brunobispoo25@gmail.com>
2023-03-15 21:47:00 -0300

    feat: Added validations to DTOs

--- src/courses_2/dto/create_course.dto.ts
+++ src/courses_2/dto/create_course.dto.ts
@@ -1,5 +1,12 @@
+import { IsString } from 'class-validator';
+
 export class CreateCourseDto {
+  @IsString()
   readonly name: string;
+
+  @IsString()
   readonly description: string;
+
+  @IsString({ each: true }) //As strings que ela vai avaliar, são as do conteudo do array
   readonly tags: string[];
 }
