6db6067e19a6c26f1e245a75d9cb62715488c1bd
Bruno Bispo <brunobispoo25@gmail.com>
2023-03-16 19:26:48 -0300

    feat: Added more validations to DTOs

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      //Remove propriedades que foram passadas no body e que não estão no DTO
+      whitelist: true,
+      //Não permitir que um registro seja criado se ele passadas no body com alguma propriedade que não esteja no DTO
+      forbidNonWhitelisted: true,
+      //Define automaticamente o tipo dos dados da requisição que chega, como o tipo do DTO
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
