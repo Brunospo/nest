a51ec35f8799c7eefb5b56776fc65c0c1af0182f
Bruno Bispo <brunobispoo25@gmail.com>
2023-04-06 21:51:53 -0300

    feat: Added database conecction

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesController } from './courses/courses.controller';
 import { Courses2Module } from './courses_2/courses_2.module';
 
 @Module({
-  imports: [Courses2Module],
+  imports: [
+    Courses2Module,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'dockerpg',
+      database: 'cursonest',
+      autoloadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, CoursesController],
   providers: [AppService],
 })
